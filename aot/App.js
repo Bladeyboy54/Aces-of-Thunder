d78425f1940bba9932b999ca3ae830652fa1a3b1
Bladeyboy54 <221146@virtualwindow.co.za>
2024-05-22 16:14:58 +0200

    Login layout fixed

--- aot/App.js
+++ aot/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './Screens/Login';
+import LoginScreen from './Screens/LoginScreen';
 
 export default function App() {
   return (
---
+++ aot/Screens/RegisterScreen.jsx
@@ -0,0 +1,2 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
